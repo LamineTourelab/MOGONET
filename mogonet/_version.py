8ca9e89546ef03c0d2fc9035e44d9c7c49592f4a
LAMINE TOURE <LAMINETOURE626@GMAIL.COM>
2025-03-06 11:53:36 +0100

    Create _version.py

---
+++ mogonet/_version.py
@@ -0,0 +1,7 @@
+# mogonet/_version.py
+
+__version__ = "0.1.0" 
+full_version = __version__
+git_revision = "unknown"  
+release = True 
+short_version = ".".join(__version__.split(".")[:0.1])  # "0.1" pour "0.1.0"
