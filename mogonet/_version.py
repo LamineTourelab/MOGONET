388520a58139cb397f812df0e4c3649c89f2a2a9
LAMINE TOURE <LAMINETOURE626@GMAIL.COM>
2025-03-19 09:16:55 +0100

    Update _version.py

--- mogonet/_version.py
+++ mogonet/_version.py
@@ -4,4 +4,4 @@ __version__ = "0.1.0"  # Version du package
 full_version = __version__
 git_revision = "unknown"  
 release = True  
-short_version = ".".join(__version__.split(".")[:0.1])  #  "0.1" pour "0.1.0"
+short_version = ".".join(__version__.split(".")[:2])  #  "0.1" pour "0.1.0"
